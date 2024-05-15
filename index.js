3d4397b6dd96706f21c4125c9ae62a0da99a814f
ibrahim <ibrahimsasmaz3416@hotmail.com>
2024-05-15 16:20:09 +0300

    env file icine compass linki eklendi

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const {getSoftwareInfo} = require("./helpers/getSoftwarInfo")
 const {getHardwareInfo} = require("./helpers/getHardwareInfo")
 
 //Connect to the mongodb and listen port 3000
-mongoose.connect(process.env.MONGO_URI).then(() => {
+mongoose.connect(process.env.MONGO_URI_COMPASS).then(() => {
     //listen for request
     app.listen(process.env.PORT, () => {
         console.log("listening on port", process.env.PORT)
