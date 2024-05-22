90d2b8dd3c523eb132a6b86ee3dd838cc782f036
ibrahim <ibrahimsasmaz3416@hotmail.com>
2024-05-22 11:07:37 +0300

    Remove Secret(access_token)

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const repo = 'hardwareImfo';
 async function checkReleases() {
     const response = await fetch(`https://api.github.com/repos/${owner}/${repo}/releases`, {
         headers: {
-            'Authorization': `token ${process.env.access_token}`
+            
         }
     });
     const releases = await response.json();
